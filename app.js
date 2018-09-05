7f5eeca10fd07b6cdd9e6b94808bc6651034f287
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-05 06:32:03 +0100

    Created function to grab html element and get integer

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ $(() => {
 
   console.log('JS Loaded');
 
+  function handleSubmit(){
+    const integer = $('input').val();
+    console.log(integer);
+  }
+
+  const $form = $('form');
+
+  $form.submit(handleSubmit);
+
 });
